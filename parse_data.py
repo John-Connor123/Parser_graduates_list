1d4b8a2fe2552756f6c4b008ed032050b68757ac
Годыно Дмитрий <digodyno@edu.hse.ru>
2022-06-12 15:31:52 +0300

    Добавлены докстринги, исправлено форматирование

--- parse_data.py
+++ parse_data.py
@@ -1,18 +1,20 @@
-__all__ = ["get_data"]
+__all__ = ["get_data", "program_breakdown"]
+
 from os import listdir, makedirs, path
 import pandas as pd
 import numpy as np
 
+
 def get_data(my_path):
-    '''
+    """
     :param my_path: путь к папке стаблицами
     :return: таблица с данными, собранными из всех таблиц
-    '''
+    """
     if not path.exists(my_path + "/csv"):
         makedirs(my_path + "/csv")
     for name in listdir(my_path):
-        WS = pd.read_excel(f"{my_path}/" + name)
-        WS.to_csv(f"{my_path}/csv" + name + ".csv", encoding="cp1251", sep = ';', index = False)
+        ws = pd.read_excel(f"{my_path}/" + name)
+        ws.to_csv(f"{my_path}/csv" + name + ".csv", encoding="cp1251", sep=';', index=False)
     students = pd.DataFrame()
     for name in listdir(my_path):
         students = students.append(func(name))
@@ -21,10 +23,10 @@ def get_data(my_path):
 
 # Эта функция достаёт данные
 def func(name):
-    '''
+    """
     :param name: название образовательной программы
     :return: таблица с необходимыми данными
-    '''
+    """
     # Достаём основную таблицу из файла CSV
     students = np.loadtxt(f"{path}/{name}",
                           delimiter=';',
@@ -32,31 +34,31 @@ def func(name):
                           encoding="cp1251",
                           dtype='O')
     # Достаём название образовательной программы
-    Text_mas = np.loadtxt(f"{path}/{name}",
+    text_mas = np.loadtxt(f"{path}/{name}",
                           delimiter=';',
                           skiprows=2,
                           encoding="cp1251",
                           max_rows=1,
                           dtype='O')
-    Napravlenie = str(Text_mas)[29:-2]
+    napravlenie = str(text_mas)[29:-2]
 
     # print(Napravlenie)
     # Достаём количество бюджетных мест
-    Text_mas = np.loadtxt(f"{path}/{name}",
+    text_mas = np.loadtxt(f"{path}/{name}",
                           delimiter=';',
                           skiprows=7,
                           encoding="cp1251",
                           max_rows=1,
                           dtype='O')
-    Budget = int(Text_mas[0][27:])
+    budget = int(text_mas[0][27:])
     # Достаём количество платных мест
-    Text_mas = np.loadtxt(f"{path}/{name}",
+    text_mas = np.loadtxt(f"{path}/{name}",
                           delimiter=';',
                           skiprows=9,
                           encoding="cp1251",
                           max_rows=1,
                           dtype='O')
-    Platno = int(Text_mas[0][25:])
+    platno = int(text_mas[0][25:])
 
     # Очистка от пустых столбцов
     for i in range(students.shape[1] - 1, 0, -1):
@@ -68,16 +70,50 @@ def func(name):
         num_of_stud += 1
     x = np.array("Образовательная программа")
     for i in range(num_of_stud, 1, -1):
-        x = np.append(x, Napravlenie)
+        x = np.append(x, napravlenie)
     students = np.c_[students, x]
     x = np.array("Бюджетные места")
     for i in range(num_of_stud, 1, -1):
-        x = np.append(x, Budget)
+        x = np.append(x, budget)
     students = np.c_[students, x]
     x = np.array("Платные места")
     for i in range(num_of_stud, 1, -1):
-        x = np.append(x, Platno)
+        x = np.append(x, platno)
     students = np.c_[students, x]
     # Запиливаем пандас датафрейм и название столбцов
-    students = pd.DataFrame(students[1:, ], columns=students[0,])
+    students = pd.DataFrame(students[1:, ], columns=students[0, ])
     return students
+
+
+def program_breakdown(students, params="", programs=[]):
+    """
+    :param students: pandas.DataFrame, база данных
+    :param params: string, 'q' - таблица по студентам, имеющим особое право,
+                    'tq' - по целевой квоте, в противном случае по олимпиадам
+    :param programs: list<string>, список образовательных программ
+    :return: pandas.DataFrame, содержащий данные по выбраной категории
+    """
+    df = students[["Образовательная программа", "Заявление о согласии на зачисление", "Возврат документов",
+                   "Литература", "Русский язык ЕГЭ", "Иностранный язык", "История ЕГЭ", "Математика ЕГЭ",
+                   "Биология ЕГЭ",
+                   "Химия", "Обществознание ЕГЭ", "Физика", "География", "Информатика", "Сумма конкурсных баллов",
+                   "Творческий конкурс Медиа", "Творческий конкурс Мода", "Творческий конкурс I этап",
+                   "Заявление о согласии на зачисление", "Возврат документов"]]
+    if params == "q":
+        column = "Поступление на места в рамках особой квоты для лиц, имеющих особое право"
+    elif params == "tq":
+        column = "Поступление на места по целевой квоте"
+    else:
+        column = "Право поступления без вступительных испытаний"
+    df[column] = students[column]
+    if programs:
+        df = df["Образовательная программа"].loc[df["Образовательная программа"].isin(programs)]
+    if params not in ["q", "tk"]:
+        df[column] = df[column].fillna("-")
+        df.loc[df[column] != "-", column] = "+"
+    new_column = pd.pivot_table(df, index=["Образовательная программа"] + [column], values="Сумма конкурсных баллов",
+                                aggfunc=len)
+    new_column.rename(columns={"Сумма конкурсных баллов": "Количество поступающих"}, inplace=True)
+    pt = pd.pivot_table(df, index=["Образовательная программа"] + [column])
+    pt = pd.concat([pt, new_column], axis=1)
+    return pt
--- work/library/work_with_stats.py
+++ work/library/work_with_stats.py
@@ -1,48 +1,26 @@
-__all__ = ['Places_for_education', 'Get_SNILS_by_exam', 'Students_Data', 'program_breakdown', 'save_file']
+__all__ = ['places_for_education', 'get_snils_by_exam', 'students_data', 'save_file']
+
+import matplotlib.figure
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from os import listdir, makedirs, path
-
-"""def get_params_to_calculate(data, education_form=''):
-'''    
-    Возвращает промежуточные данные для расчётов.
-    :param data: pandas-обьект с данными по приемной кампании.
-    :param education_form: форма обучения.
-    :return: список из суммы баллов по ЕГЭ для каждого абитуриента;
-    список, содержащий информацию о том, подал ли заявление каждый абитуриент;
-    кол-во олимпиадников; общее кол-во конкурсных мест.
-    '''
-    return final_scores, agreements, BVI_number, all_places
 
 
-def get_stats(data, education_form=''):
-    ''':param data: pandas-обьект с данными по приемной кампании
-       :param education_form: форма обучения
-       :return: средний балл и минимальный балл для платников'''
-    final_scores, agreements, BVI_number, all_places = get_params_to_calculate(data, education_form=education_form)
-
-    return avg_score_commerce, min_score_commerce
-"""
-
-
-def Get_SNILS_by_exam(students, exam, points, operator = '>'):
-    '''
+def get_snils_by_exam(students, exam, points, operator='>'):
+    """
     :param students: pandas.DataFrame, База данных
     :param exam: string, Экзамен, по которому будет построена статистика
     :param points: integer, Количество баллов, относительно которого рассматривается статистика
     :param operator: char, '<', '>' или '='. Необходим для показа балов ниже, выше или равных порога (по умолчанию '>')
     :return: таблицу со СНИЛСами и данными по заданному экзамену, картинку с двумя графиками:
     круговой диаграммой и ящиком с усами
-    '''
+    """
     counts = {"< " + str(points): 0, "= " + str(points): 0, "> " + str(points): 0}
     df = students[["СНИЛС", exam]]
     df = df[~df[exam].isnull()]
     df = df[df[exam] != ""]
     df[exam] = df[exam].astype(int)
-    plt.subplot(2,1,2)
-    df[[exam]].boxplot(grid = True, notch = True, showmeans = True, whis = 1.5, vert = False, showfliers = False)
+    plt.subplot(2, 1, 2)
+    df[[exam]].boxplot(grid=True, notch=True, showmeans=True, whis=1.5, vert=False, showfliers=False)
     for score in df[exam]:
         if score < points:
             counts["< " + str(points)] += 1
@@ -56,114 +34,96 @@ def Get_SNILS_by_exam(students, exam, points, operator = '>'):
         df = df[df[exam] == points]
     else:
         df = df[df[exam] > points]
-    df.reset_index(inplace = True, drop = True)
-    plt.subplot(2,1,1)
-    plt.pie(counts.values(), labels = counts.keys())
+    df.reset_index(inplace=True, drop=True)
+    plt.subplot(2, 1, 1)
+    plt.pie(counts.values(), labels=counts.keys())
     plt.title("Количество баллов по предмету " + exam)
     plt.ylabel("")
     return df, plt
 
 
-def Places_for_education(students, programs, budget, paid, programStr = "Образовательная программа", budgetStr = "Бюджетные места", paidStr ="Платные места"):
-    '''
+def places_for_education(students, programs, budget, paid, program_str="Образовательная программа",
+                         budget_str="Бюджетные места", paid_str="Платные места"):
+    """
     :param students: pandas.DataFrame, база данных
     :param programs: list<string>, список образовательных программ
     :param budget: char, '+' - сортировать по количеству бюджетных мест, '-' - не сортировать
     :param paid: char, '+' - сортировать по количеству бюджетных мест, '-' - не сортировать
-    :param programStr: string, название столбца базы данных, в котором написаны названия образовательных программ
-    :param budgetStr: string, название столбца базы данных, в котором написаны количества бюджетных мест
-    :param paidStr: string, название столбца базы данных, в котором написаны количества платных мест
+    :param program_str: string, название столбца базы данных, в котором написаны названия образовательных программ
+    :param budget_str: string, название столбца базы данных, в котором написаны количества бюджетных мест
+    :param paid_str: string, название столбца базы данных, в котором написаны количества платных мест
     :return: таблица с количеством платных и/или бюджетных мест по выбраным программам
     и столбчатую диаграмму с количеством платных и бюджетных мест по выбранным программам
-    '''
-    df = students[["Образовательная программа", "Бюджетные места", "Платные места"]]
+    """
+    df = students[[program_str, budget_str, paid_str]]
     if budget == '-':
         if paid == '-':
-            df = df[["Образовательная программа"]]
+            df = df[[program_str]]
         else:
-            df = df[["Образовательная программа", "Платные места"]]
+            df = df[[program_str, paid_str]]
     elif paid == '-':
-        df = df[["Образовательная программа", "Бюджетные места"]]
-    df = df.drop_duplicates(subset = "Образовательная программа", keep = "first")
+        df = df[[program_str, budget_str]]
+    df = df.drop_duplicates(subset=program_str, keep="first")
     if programs:
-        df = df.loc[df["Образовательная программа"].isin(programs)]
-    df.reset_index(inplace = True, drop = True)
-    df = df.set_index("Образовательная программа")
-    hist1 = df.plot(figsize=(26,25),kind="bar")
+        df = df.loc[df[program_str].isin(programs)]
+    df.reset_index(inplace=True, drop=True)
+    df = df.set_index(program_str)
+    hist1 = df.plot(figsize=(26, 25), kind="bar")
     fig1 = hist1.get_figure()
     return df, fig1
 
 
-def Students_Data(students,  without_exam, special_q, target_q, programs, points,
-                  operator, without_examStr = "Право поступления без вступительных испытаний",
-                  special_qStr = "Поступление на места в рамках особой квоты для лиц, имеющих особое право",
-                  target_qStr = "Поступление на места по целевой квоте", programStr = "Образовательная программа",
-                  points_sumStr = "Сумма конкурсных баллов"):
-    '''
+def students_data(students, without_exam, special_q, target_q, programs, points,
+                  operator, without_exam_str="Право поступления без вступительных испытаний",
+                  special_quota_str="Поступление на места в рамках особой квоты для лиц, имеющих особое право",
+                  target_quota_str="Поступление на места по целевой квоте", program_str="Образовательная программа",
+                  points_sum_str="Сумма конкурсных баллов"):
+    """
     :param students: pandas.DataFrame, база данных
-    :param without_exam: char, '+' - сортировать по олимпиадам, '-' - не сортировать
-    :param special_q: char, '+' - сортировать по специальной квоте, '-' - не сортировать
-    :param target_q: char, '+' - сортировать по целевой квоте, '-' - не сортировать
+    :param without_exam: string, '+' - сортировать по олимпиадам, '-' - не сортировать
+    :param special_q: string, '+' - сортировать по специальной квоте, '-' - не сортировать
+    :param target_q: string, '+' - сортировать по целевой квоте, '-' - не сортировать
     :param programs: list<string>, список образовательных программ
     :param points: integer, Количество баллов, относительно которого рассматривается статистика
-    :param operator: char, '<', '>' или '='. Необходим для показа балов ниже, выше или равных порога (по умолчанию '>')
-    :param without_examStr: string, название столбца базы данных, в котором написаны права на поступление по олимпиаде
-    :param special_qStr: string, название столбца базы данных, в котором написаны права на поступление по специальной квоте
-    :param target_qStr: string, название столбца базы данных, в котором написаны права на поступление по целевой квоте
-    :param programStr: string, название столбца базы данных, в котором написаны названия образовательных программ
-    :param points_sumStr: string, название столбца базы данных, в котором написаны суммы конкурсных баллов
+    :param operator: string, '<', '>' или '='. Необходим для показа балов ниже, выше или равных порога
+    :param without_exam_str: string, название столбца с данными о студентах с правом на поступление по олимпиаде
+    :param special_quota_str:string, название столбца с данными о студентах с правом на поступление по специальной квоте
+    :param target_quota_str: string, название столбца с данными о студентах с правом на поступление по целевой квоте
+    :param program_str: string, название столбца базы данных, в котором написаны названия образовательных программ
+    :param points_sum_str: string, название столбца базы данных, в котором написаны суммы конкурсных баллов
     :return:
-    '''
-    df = students[["СНИЛС", without_examStr, special_qStr, target_qStr, programStr, points_sumStr]]
-    df = df[~df[points_sumStr].isnull()]
-    df = df[df[points_sumStr] != ""]
-    df[points_sumStr] = df[points_sumStr].astype(int)
+    """
+    df = students[["СНИЛС", without_exam_str, special_quota_str, target_quota_str, program_str, points_sum_str]]
+    df = df[~df[points_sum_str].isnull()]
+    df = df[df[points_sum_str] != ""]
+    df[points_sum_str] = df[points_sum_str].astype(int)
     if programs:
-        buff = df[programStr].loc[~df[programStr].isin(programs)]
-        df = df.loc[~df[programStr].isin(buff)]
+        buff = df[program_str].loc[~df[program_str].isin(programs)]
+        df = df.loc[~df[program_str].isin(buff)]
     if operator == '<':
-        df = df[df[points_sumStr] < points]
+        df = df[df[points_sum_str] < points]
     elif operator == '=':
-        df = df[df[points_sumStr] == points]
+        df = df[df[points_sum_str] == points]
     else:
-        df = df[df[points_sumStr] > points]
-    df1 = df[["СНИЛС", programStr, points_sumStr]]
+        df = df[df[points_sum_str] > points]
+    df1 = df[["СНИЛС", program_str, points_sum_str]]
     if without_exam == '+':
-        df1.loc[:,without_examStr] = df[without_examStr]
+        df1.loc[:, without_exam_str] = df[without_exam_str]
     if special_q == '+':
-        df1.loc[:,special_qStr] = df[special_qStr]
+        df1.loc[:, special_quota_str] = df[special_quota_str]
     if target_q == '+':
-        df1.loc[:, target_qStr] = df[target_qStr]
+        df1.loc[:, target_quota_str] = df[target_quota_str]
     return df1
 
 
-def program_breakdown(students, params = "", programs = []):
-    df = students[["Образовательная программа","Заявление о согласии на зачисление","Возврат документов",
-                  "Литература","Русский язык ЕГЭ","Иностранный язык","История ЕГЭ","Математика ЕГЭ","Биология ЕГЭ",
-                  "Химия","Обществознание ЕГЭ","Физика","География","Информатика","Сумма конкурсных баллов",
-                  "Творческий конкурс Медиа","Творческий конкурс Мода","Творческий конкурс I этап",
-                  "Заявление о согласии на зачисление","Возврат документов"]]
-    if params == "q":
-        column = "Поступление на места в рамках особой квоты для лиц, имеющих особое право"
-    elif params == "tq":
-        column = "Поступление на места по целевой квоте"
-    else:
-        column = "Право поступления без вступительных испытаний"
-    df[column] = students[column]
-    if programs:
-        df = df["Образовательная программа"].loc[df["Образовательная программа"].isin(programs)]
-    if params not in ["q","tk"]:
-        df[column] = df[column].fillna("-")
-        df.loc[df[column] != "-", column] = "+"
-    new_column = pd.pivot_table(df, index = ["Образовательная программа"] + [column], values = "Сумма конкурсных баллов", aggfunc = len)
-    new_column.rename(columns = {"Сумма конкурсных баллов": "Количество поступающих"}, inplace = True)
-    pt = pd.pivot_table(df, index = ["Образовательная программа"] + [column])
-    pt = pd.concat([pt,new_column], axis = 1)
-    return pt, new_column
-
-
 def save_file(file, file_name, path=""):
+    """
+    :param file: string, переменная, содержащая данные, которые необходимо сохранить
+    :param file_name: string, имя для будущего файла. Без расширения
+    :param path: string, путь к папке для сохранения файла
+    :return: файл в формате .png или .xlsx
+    """
     if isinstance(file, pd.DataFrame):
         file.to_excel(path + file_name + ".xlsx")
-    else:
+    elif isinstance(file, matplotlib.figure.Figure):
         file.savefig(path + file_name + ".png")
