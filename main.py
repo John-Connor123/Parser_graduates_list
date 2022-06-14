fcfc84b205aa6960c61b48cd5cdba50911a7ab10
dsdublenskiy <dsdublenskiy@miem.hse.ru>
2022-06-14 23:10:08 +0300

    Add DataBase in new file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Combine_Interface_with_pandas
 
 Combine_Interface_with_pandas.start_menu()
 pril = Combine_Interface_with_pandas.bot_or_pril
-if not pril:
+if pril == "0":
     Combine_Interface_with_pandas.info_telegram_bot()
-else:
+elif pril == "1":
     Combine_Interface_with_pandas.run_app()
